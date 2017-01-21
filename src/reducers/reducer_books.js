bb65fd2b1135586d462dcfa4726b090164ddca1d
Sebo Kim <sanari85@gmail.com>
2017-01-21 15:09:31 -0500

    init

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+
+export default function(state=null, action) {
+  return [
+    {title: 'Javascript: The good part.', pages: 101},
+    {title: 'Harry Potter', pages: 30},
+    {title: 'The Dark Tower', pages:2},
+  ]
+}
